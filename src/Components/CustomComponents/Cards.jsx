7f142848c31a3e143faf6dd06bce78783e514511
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2024-11-19 15:35:26 +0530

    Formated and Added DeleteIconButton

--- src/Components/CustomComponents/Cards.jsx
+++ src/Components/CustomComponents/Cards.jsx
@@ -1,48 +1,64 @@
 import React from "react";
-import './cards.css'
+import "./cards.css";
 import StarRating from "./Rating";
+import { DeleteIconButton } from "./CustomButtons";
 
 export const ImageCard = ({ imageName, imageDescription }) => {
-    return (
-        <div className="Image-Card">
-            <div className="ImageBox">
-                <img src={imageName + ".png"} alt="" />
-            </div>
-            <div className="ImageContent">
-                <h2>{imageName}</h2>
-                <p>{imageDescription}</p>
-            </div>
+  return (
+    <div className="Image-Card">
+      <div className="ImageBox">
+        <img src={imageName + ".png"} alt="" />
+      </div>
+      <div className="ImageContent">
+        <h2>{imageName}</h2>
+        <p>{imageDescription}</p>
+      </div>
+    </div>
+  );
+};
+
+export const WishListCard = ({ product, onDeleteIconClick }) => {
+  return (
+    <div className="WishListCard">
+      <div className="WishListCardImage">
+        <img
+          src={product.productImageUrl[0]}
+          alt={product.productImageUrl[0]}
+        />
+      </div>
+      <div className="WishListCardDetails">
+        <div className="WishListName">{product.productName}</div>
+        <div className="WishListRating">
+          Rating : <StarRating rating={product.productRating} />
         </div>
-    )
-}
+        <div className="WishListCost">Cost : Rs. {product.productCost}</div>
+      </div>
+      <div className="MenuConatiner">
+        <DeleteIconButton onClick={onDeleteIconClick} />
+      </div>
+    </div>
+  );
+};
 
-
-export const WishListCard = ({ product }) => {
-    return (
-        <div className="WishListCard">
-            <div className="WishListCardImage">
-                <img src={product.productImageUrl[0]} alt={product.productImageUrl[0]} />
-            </div>
-            <div className="WishListCardDetails">
-                <div className="WishListName">{product.productName}</div>
-                <div className="WishListRating">Rating : <StarRating rating={product.productRating} /></div>
-                <div className="WishListCost">Cost : Rs. {product.productCost}</div>
-            </div>
+export const CartListCard = ({ product, onDeleteIconClick }) => {
+  return (
+    <div className="CartListCard">
+      <div className="CartListCardImage">
+        <img
+          src={product.productImageUrl[0]}
+          alt={product.productImageUrl[0]}
+        />
+      </div>
+      <div className="CartListtCardDetails">
+        <div className="CartListName">{product.productName}</div>
+        <div className="CartListRating">
+          Rating : <StarRating rating={product.productRating} />
         </div>
-    )
-}
-
-
-export const CartListCard = ({ product }) => {
-    return (
-        <div className="CartListCard">
-            <div className="CartListCardImage">
-                <img src={product.productImageUrl[0]} alt={product.productImageUrl[0]} />
-            </div>
-            <div className="CartListtCardDetails">
-                <div className="CartListName">{product.productName}</div>
-                <div className="CartListRating">Rating : <StarRating rating={product.productRating} /></div>
-                <div className="CartListCost">Cost : Rs. {product.productCost}</div>
-            </div>
-        </div>)
-}
+        <div className="CartListCost">Cost : Rs. {product.productCost}</div>
+      </div>
+      <div className="MenuConatiner">
+        <DeleteIconButton onClick={onDeleteIconClick} />
+      </div>
+    </div>
+  );
+};
